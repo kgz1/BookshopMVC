78ba3ccb76ee1e2fe1fee7e43e60f70d1d6850f1
kgz1 <kg034269@gmail.com>
2024-07-05 02:43:36 +0200

    commit

--- obj/Debug/net8.0/BookShopByKg.AssemblyInfo.cs
+++ obj/Debug/net8.0/BookShopByKg.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookShopByKg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+404d8051e041ba8a6fc105565094f05530369c03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12824f70abcb2c16f63b61f03884f853b0bcd360")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookShopByKg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookShopByKg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
